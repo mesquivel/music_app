e573f826ac6c2bcfeb3f297d380a0efb3f3a0a6a
Andres Gatjens <andresgg@criticalmass.com>
2017-07-23 18:45:20 -0600

    add test router component example

---
+++ src/components/RouteTestComponent/RouteTestComponent.vue
@@ -0,0 +1,6 @@
+<template src="./template.html"></template>
+
+<script src="./script.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped src="./style.scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import TestComponent from '@/components/TestComponent/TestComponent'
+import RouteTestComponent from '@/components/RouteTestComponent/RouteTestComponent'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'TestComponent',
       component: TestComponent
+    },
+    {
+      path: '/about',
+      name: 'RouteTestComponent',
+      component: RouteTestComponent
     }
   ]
 })
