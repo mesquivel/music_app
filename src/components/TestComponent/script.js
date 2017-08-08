9b94b09fbd9ff8946a533cc278ffd544d9cd06d8
Andres Gatjens <andresgg@criticalmass.com>
2017-08-08 11:47:13 -0600

    add svg loader and example

--- src/components/TestComponent/script.js
+++ src/components/TestComponent/script.js
@@ -1,8 +1,13 @@
+import facebookIcon from '@/assets/svg/icon-facebook.svg';
+import mediumIcon from '@/assets/svg/icon-medium.svg';
+
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App TestComponent'
+      msg: 'Welcome to Your Vue.js App TestComponent',
+      facebookIcon: `<svg class="icon-facebook" viewBox="${facebookIcon.viewBox}"><use xlink:href="#${facebookIcon.id}" /></svg>`,
+      mediumIcon: `<svg class="icon-medium" viewBox="${mediumIcon.viewBox}"><use xlink:href="#${mediumIcon.id}" /></svg>`
     }
   }
 }
